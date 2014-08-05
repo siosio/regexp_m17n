6c32400d995b3478bb11e10cdc10c884b451f737
siosio <siosio12@gmail.com>
2014-08-05 16:20:12 +0900

    support dummy encoding.
    dummy encoding used 'string#fource_encoding'

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,15 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str.encode('utf-8', str.encoding) =~ /^.+$/
+    to_utf8_str(str) =~ /^.+$/
   end
+
+  private
+  def self.to_utf8_str(str)
+    if str.encoding.dummy?
+      str.force_encoding('utf-8')
+    else
+      str.encode('utf-8', str.encoding)
+    end
+  end
+
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,8 +5,16 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      next if enc.dummy?
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      assert(RegexpM17N.non_empty?(make_data(enc)))
+    end
+  end
+
+  private
+  def make_data(enc)
+    if enc.dummy?
+      '.'.force_encoding(enc)
+    else
+      '.'.encode(enc)
     end
   end
 end
